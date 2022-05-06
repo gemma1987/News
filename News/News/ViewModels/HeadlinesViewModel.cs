68b3958eda8815137102d3859f8bce2f7b9c7ae3
Gemma Neeleman <gemma.neeleman@gmail.com>
2022-05-06 13:46:14 +0200

    Add project files.

---
+++ News/News/ViewModels/HeadlinesViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace News.ViewModels
+{
+    internal class HeadlinesViewModel : ViewModel
+    {       
+        public HeadlinesViewModel()
+        {
+        }
+    }
+}
